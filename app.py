ba1d90dee3de943d4eb7ccef0119c74fd91f0581
Chesterwongz <psyforce99@gmail.com>
2021-08-15 23:12:47 +0800

    basic flask setup
    form settings and uploaded image display done

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Set max file size
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+def listToString(list):
+    str1 = ""
+    for ele in list:
+        str1 = str1 + " " + ele
+    return str1
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are -' + listToString(ALLOWED_EXTENSIONS))
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    flash('File size must be below 16MB')
+    return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
