65fe9c6882f48bf292dac25d17ec034545fcd46e
Chesterwongz <psyforce99@gmail.com>
2021-08-31 23:20:57 +0800

    no server implementation

--- app.py
+++ app.py
@@ -1,73 +1,39 @@
 # Root directory of the project
+from mrcnn.model import MaskRCNN
 import os
 import sys
+import base64
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 # Import Mask RCNN
 ROOT_DIR = os.path.abspath("./Mask_RCNN")
 
 sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn.model import MaskRCNN
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = 'static/uploads/'
-
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# Set max file size
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-
-
-def listToString(list):
-    str1 = ""
-    for ele in list:
-        str1 = str1 + " " + ele
-    return str1
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('testing.html')
 
-# This implementation requires a server
-# An alternative is using data uri, not sure how to make it work with model predict:
-# https://buraksenol.medium.com/pass-images-to-html-without-saving-them-as-files-using-python-flask-b055f29908a
+
 @app.route('/', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
-        flash('No file part')
+        print('No file part')
         return redirect(request.url)
     file = request.files['file']
     if file.filename == '':
-        flash('No image selected')
+        print('No image selected')
         return redirect(request.url)
-    if file and allowed_file(file.filename):
+    if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
-        return render_template('index.html', filename=filename)
+        image_string = base64.b64encode(file.stream.read())
+        print('upload_image filename: ' + filename)
+        return render_template('testing.html', filename=filename)
     else:
-        flash('Allowed image types are -' + listToString(ALLOWED_EXTENSIONS))
         return redirect(request.url)
-
-
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
-
-@app.errorhandler(413)
-def request_entity_too_large(error):
-    flash('File size must be below 16MB')
-    return redirect(request.url)
 
 
 if __name__ == "__main__":
