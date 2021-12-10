15fdaf2ec728ce08e357ef55692fd0479f4a3494
Amadeus <amadeusw6@gmail.com>
2021-12-10 16:49:58 +0800

    Refactored scripts for models

--- scripts/typedefs.py
+++ scripts/typedefs.py
@@ -3,9 +3,10 @@ from typing import TypeVar, Sequence, Generic, List, Tuple
 import numpy.typing as npt
 
 Image = npt.ArrayLike
+Mask = npt.ArrayLike
 
-T = TypeVar('T')
-npArray = Sequence[T] 
+S = TypeVar('S')
+npArray = Sequence[S] 
 
 class BoundingBox:
     """ A Class representing a bounding box
@@ -26,6 +27,15 @@ class BoundingBox:
     ymin: float
     xmax: float
     ymax: float
+
+    def __init__(self, xmin: float, ymin: float, xmax: float, ymax: float) -> None:
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+    
+    def __str__(self) -> str:
+        return "{xmin: " + str(self.xmin) + ", ymin: " + str(self.ymin) + ", xmax: " + str(self.xmax) + ", ymax: " + str(self.ymax) + "}" 
 
 class DetectionPrediction:
     """ Parent class of all model predictions.
@@ -50,7 +60,11 @@ class DetectionPrediction:
 T = TypeVar("T", bound=DetectionPrediction)
 class Model(Generic[T], metaclass=ABCMeta):
     """ Parent class of all models
+
+    The website makes use of these methods to format prediction. Thus, any model to be used
+    with the website must inherit from this class. 
     """
+
     @abstractmethod
     def predict_batch(self, imgs:List[Image]) -> Tuple[npArray[Image], npArray[Image], List[T]]:
         pass
