3a5ae81a66151a9f92f5aa475d76957d26a6194d
Ubuntu <ubuntu@ip-172-31-65-95.ec2.internal>
2022-09-20 16:49:17 +0000

    Fixed website for SOC Homecoming presentation

--- Website_Docker_Homecoming/scripts/models/YoloModel.py
+++ Website_Docker_Homecoming/scripts/models/YoloModel.py
@@ -29,7 +29,8 @@ class YoloModel(Model[T]):
     def predict_batch(self, imgs:List[Image]) -> Tuple[npArray[Image], npArray[Image], List[List[T]]]:
         results = self.model(imgs)
         
-        original_imgs : npArray[Image]  = np.copy(results.imgs)
+        # original_imgs : npArray[Image]  = np.copy(results.imgs)
+        original_imgs : npArray[Image]  = np.copy(imgs)
         processed_imgs: npArray[Image]  = results.render()
         detections = results.pandas().xyxy # Contains coordinates of the detection
 
@@ -47,26 +48,6 @@ class YoloModel(Model[T]):
         return original_imgs, processed_imgs, predictions
 
     def predict_single(self, img:Image) -> Tuple[Image, Image, List[T]]:
-        results = self.model(imgs)
+        original_imgs, processed_imgs, predictions = self.predict_batch([img])
         
-        original_imgs : Image  = img
-        processed_imgs: results.render()
-        detections = results.pandas().xyxy # Contains coordinates of the detection
-
-        predictions = []
-        for detection in detections:
-            detection = json.loads(detection.to_json(orient="records"))
-            prediction = []
-            for det in detection:
-                class_id = det["name"]
-                score = det["confidence"]
-                bbox = self._extractBboxes(detection=det)
-                prediction.append(YoloPrediction(bbox, score, class_id))
-            predictions.append(prediction)
-
-        return original_imgs, processed_imgs, predictions
-
-
-        # original_imgs, processed_imgs, predictions = self.predict_batch([img])
-        
-        # return original_imgs[0], processed_imgs[0], predictions[0]
+        return original_imgs[0], processed_imgs[0], predictions[0]
